18e1a0ac614bb46c2902ed132a4d117e44af616d
Emeka-Chukwu <emekapascal11@gmail.com>
2022-08-20 13:47:32 +0100

    mailer service

---
+++ mail-service/cmd/api/routes.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+	"github.com/go-chi/cors"
+)
+
+func (app *Config) routes() http.Handler {
+	mux := chi.NewRouter()
+
+	// specify who is allowed to connect
+	mux.Use(cors.Handler(cors.Options{
+		AllowedOrigins:   []string{"https://localhost", "http://localhost"},
+		AllowedMethods:   []string{"GET", "PUT", "POST", "DELETE", "OPTIONS"},
+		AllowedHeaders:   []string{"Aceept", "Authorization", "Content-Type", "X-CSRF-Token"},
+		ExposedHeaders:   []string{"Link"},
+		AllowCredentials: true,
+		MaxAge:           300,
+		Debug:            false,
+	}))
+
+	mux.Use(middleware.Heartbeat("/ping"))
+	mux.Post("/send", app.SendMail)
+
+	return mux
+
+}
